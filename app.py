14715db4d24be3a5ef43d400af9a2990be8f495e
asad_62 <asaduet2662@gmail.com>
2025-06-07 22:00:19 +0200

    updated

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from inference import predict
 import io
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/predict", response_class=HTMLResponse)
 async def predict_route(request: Request, file: UploadFile = File(...)):
     image_bytes = await file.read()
     result = predict(image_bytes)
+
+    # Return JSON if client requests it (e.g., curl or script)
+    if "application/json" in request.headers.get("accept", ""):
+        return JSONResponse(content={"prediction": result})
+
+    # Else return HTML page
     return templates.TemplateResponse("index.html", {
         "request": request,
         "result": result
     })
+
