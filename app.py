4b75414cf974351d5528ea55b1185962c9aa5287
asad_62 <asaduet2662@gmail.com>
2025-06-07 22:07:26 +0200

    updated

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 @app.post("/predict", response_class=HTMLResponse)
 async def predict_route(request: Request, file: UploadFile = File(...)):
     image_bytes = await file.read()
